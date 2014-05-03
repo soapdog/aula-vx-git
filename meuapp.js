f938ca127c05013c0a17c5782ab5699fd249f083
Andre Alves Garzia <contato@andregarzia.com>
2014-05-03 13:09:41 -0300

    crypto

--- meuapp.js
+++ meuapp.js
@@ -1,20 +1,48 @@
-function meleca() {
-  console.log("meleca!");
+data = {};
+
+function registraUsuario(usuario, senha) {
+  data.usuario = usuario;
+  data.pass = senha;
 }
 
-function login() {
+function api(obj, callback) {
+
+  var seguro = data.pass + "qualquercoisa";
+
+  var dados = {
+    usuario: data.usuario,
+    pass: CryptoJS.SHA1(seguro).toString(CryptoJS.enc.Hex)
+  };
+
+  console.log("enviando", dados);
+
   $.ajax({
     url: "http://labs.vxcom.me/aula/aula.php",
     dataType: "json",
-    method: "GET",
-    success: function(data, status, xhr) {
-      alert(data.status);
+    method: "POST",
+    data: dados,
+    success: function(resposta, status, xhr) {
+      console.log(resposta);
+      if (resposta.status === "erro") {
+        alert("Erro:" + resposta.msg);
+      } else {
+        localStorage.setItem("data", JSON.stringify(data));
+        console.log(data);
+      }
     }
   });
 }
 
 $(document).ready(function() {
+
+  var t = localStorage.getItem("data");
+
+  if (t) {
+    data = JSON.parse(t);
+  }
+
+
   $("#botao").click(function() {
-    login();
+    api();
   });
 });
